a861d7e75401e9b3883eb6f550a6df2775e66be8
DEViousSeagull <86694079+DEViousSeagull@users.noreply.github.com>
2025-10-15 10:30:35 -0300

    test navegacion

--- material_practico/trabajos_practicos_evaluables/tp_06/frontend/src/app/pages/home/home.spec.ts
+++ material_practico/trabajos_practicos_evaluables/tp_06/frontend/src/app/pages/home/home.spec.ts
@@ -1,19 +1,33 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router';
 import { By } from '@angular/platform-browser';
 import { Home } from './home';
+import { Location } from '@angular/common';
+import { Routes } from '@angular/router';
+import { ComprarEntrada } from '../comprar-entrada/comprar-entrada';
 
 describe('Home', () => {
     let component: Home;
     let fixture: ComponentFixture<Home>;
+    let router: Router;
+    let location: Location;
+
+    const routes: Routes = [
+        { path: 'comprar-entrada', component: ComprarEntrada }
+    ];
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            imports: [Home, RouterTestingModule],
+            imports: [Home, RouterTestingModule.withRoutes(routes)],
         }).compileComponents();
+
+        router = TestBed.inject(Router);
+        location = TestBed.inject(Location);
 
         fixture = TestBed.createComponent(Home);
         component = fixture.componentInstance;
+        router.initialNavigation();
         fixture.detectChanges();
     });
 
@@ -21,9 +35,10 @@ describe('Home', () => {
         expect(component).toBeTruthy();
     });
 
-    it('should have a button to navigate to comprar-tickets', () => {
-        const button = fixture.debugElement.query(By.css('button'));
-        expect(button).toBeTruthy();
-        expect(button.nativeElement.textContent).toContain('Comprar Entrada');
+    it('should navigate to /comprar-entrada when button is clicked', async () => {
+        const button = fixture.debugElement.query(By.css('button')).nativeElement;
+        button.click();
+        await fixture.whenStable(); 
+        expect(location.path()).toBe('/comprar-entrada');
     });
 });

da00b7f2e091f7ad2be035868d472133c61f3bf2
DEViousSeagull <86694079+DEViousSeagull@users.noreply.github.com>
2025-10-21 16:19:19 -0300

    changes front

--- material_practico/trabajos_practicos_evaluables/tp_06/frontend/src/app/app.routes.ts
+++ material_practico/trabajos_practicos_evaluables/tp_06/frontend/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { ComprarEntrada } from './pages/comprar-entrada/comprar-entrada';
+import { MisEntradas } from './pages/mis-entradas/mis-entradas';
 
 export const routes: Routes = [
     {
@@ -11,6 +12,10 @@ export const routes: Routes = [
     {
         path: 'comprar-entrada',
         component: ComprarEntrada,
+    },
+    {
+        path: 'mis-entrada',
+        component: MisEntradas,
     },
     {
         path: '**',
---
+++ material_practico/trabajos_practicos_evaluables/tp_06/frontend/src/app/pages/mis-entradas/mis-entradas.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-mis-entradas',
+    templateUrl: './mis-entradas.html',
+    styleUrls: ['./mis-entradas.scss'],
+    standalone: true,
+    imports: []
+})
+export class MisEntradas {
+    
+}

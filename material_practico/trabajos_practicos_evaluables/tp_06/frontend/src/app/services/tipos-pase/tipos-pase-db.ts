3e42bb6a3f3ddf3d44958ea65f1b25e51f00906e
DEViousSeagull <86694079+DEViousSeagull@users.noreply.github.com>
2025-10-21 15:03:18 -0300

    tipos db

--- material_practico/trabajos_practicos_evaluables/tp_06/frontend/src/app/services/tipos-pase/tipos-pase-db.ts
+++ material_practico/trabajos_practicos_evaluables/tp_06/frontend/src/app/services/tipos-pase/tipos-pase-db.ts
@@ -1,8 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+export interface TipoEntrada {
+    id: number;
+    nombre: string;
+}
 
 @Injectable({
     providedIn: 'root'
 })
 export class TiposPaseDb {
+    private baseUrl = 'http://localhost:8000/tipos-entrada';
 
+    constructor(private httpClient: HttpClient) { }
+
+    getAll(): Observable<TipoEntrada[]> {
+        return this.httpClient.get<TipoEntrada[]>(this.baseUrl);
+    }
 }
